528734b2c7434d4d072a0f0ecf7ac14f688b149d
Eslam A. Hefnawy <eahefnawy@gmail.com>
2015-09-12 16:11:34 +0300

    specified versions

--- package.js
+++ package.js
@@ -1,22 +1,21 @@
 Package.describe({
   name: 'eahefnawy:accounts-coinbase',
   summary: 'A Meteor loginWithCoinbase Functionality.',
-  version: '0.0.1',
+  version: '0.0.1-plugins.0',
   git: 'https://github.com/eahefnawy/accounts-coinbase.git',
 });
 
 Package.onUse(function(api) {
-  api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('http', ['server']);
-  api.use('templating', 'client');
-  api.use('underscore', 'server');
-  api.use('random', 'client');
-  api.use('service-configuration', ['client', 'server']);
-  api.use('accounts-base', ['client', 'server']);
+  api.use('oauth2@1.1.3', ['client', 'server']);
+  api.use('oauth@1.1.4', ['client', 'server']);
+  api.use('http@1.1.0', ['server']);
+  api.use('underscore@1.0.3', 'server');
+  api.use('random@1.0.3', 'client');
+  api.use('service-configuration@1.0.4', ['client', 'server']);
+  api.use('accounts-base@1.2.0', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
+  api.use('accounts-oauth@1.1.5', ['client', 'server']);
 
   api.export('Coinbase');
 
